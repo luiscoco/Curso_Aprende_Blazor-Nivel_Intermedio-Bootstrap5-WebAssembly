15db57a487cfae504358c36bb183e377e9f34b03
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-10-04 06:02:10 +0200

    Agregar archivos de proyecto.

---
+++ wwwroot/accordion.js
@@ -0,0 +1,11 @@
+﻿function toggleAccordion(action) {
+    let accordionItems = document.querySelectorAll('.accordion-collapse');
+
+    accordionItems.forEach(item => {
+        if (action === 'collapse') {
+            item.classList.remove('show');  // Collapse all
+        } else if (action === 'expand') {
+            item.classList.add('show');  // Expand all
+        }
+    });
+}
